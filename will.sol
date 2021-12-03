3a699e8fd502f225d6349b909bd662fbad7ea3fe
jupiter229 <jupiterit29@gmail.com>
2021-12-02 18:33:21 -0800

    send different ether to different accounts

--- will.sol
+++ will.sol
@@ -11,4 +11,36 @@ contract Will {
         deceased = false;
     }    
 
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    modifier mustBeDeceased {
+        require(deceased == true);
+        _;
+    }
+
+    address payable[] familyWallets;
+
+    mapping(address => uint) inheritance;
+
+    function setInheritance(address payable wallet, uint amount) public {
+        familyWallets.push(wallet);
+        inheritance[wallet] = amount;
+    }
+
+    //pay each family memeber based on their wallet address
+
+    function payout() private mustBeDeceased {
+        for(uint i = 0; i < familyWallets.length; i++) {
+            familyWallets[i].transfer(inheritance[familyWallets[i]]);
+        }
+    }
+
+    //oracle switch
+    function hasDeceased() public onlyOwner {
+        deceased = true;
+        payout();
+    }
 }
