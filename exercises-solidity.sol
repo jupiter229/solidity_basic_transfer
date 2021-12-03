463958546fd5e684c54984f419b8eb7cf14bce2c
no-ki <nolanit229@gmail.com>
2021-12-02 17:54:42 -0800

    basic knowledge with solidity

---
+++ exercises-solidity.sol
@@ -0,0 +1,13 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract simpleStorage {
+    uint storeData;
+
+    function set(uint x) public {
+        storeData = x * 5;
+    }
+
+    function get() public view returns (uint) {
+        return storeData;
+    }
+}
